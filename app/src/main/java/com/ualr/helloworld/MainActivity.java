6eca0d9ae4ff4e8c374c62cc76218945e5bd3ab3
irconde <ivanrodriguezconde@gmail.com>
2019-07-19 11:09:47 +0200

    show button click action

--- app/src/main/java/com/ualr/helloworld/MainActivity.java
+++ app/src/main/java/com/ualr/helloworld/MainActivity.java
@@ -1,14 +1,26 @@
 package com.ualr.helloworld;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private TextView userMsgTV;
+    private EditText userInputET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userMsgTV = findViewById(R.id.userMsgTV);
+        userInputET = findViewById(R.id.userInputET);
+    }
+
+    public void showTextMessage(View view) {
+        userMsgTV.setText(userInputET.getText().toString());
     }
 }
